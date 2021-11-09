37c739cecd819b0936bf31df4e85f3d80ec9697b
Benniswar <92287642+Benniswar@users.noreply.github.com>
2021-11-09 10:28:01 +0530

    Create ASCII VALUES OF NUMBERS.c

---
+++ ASCII VALUES OF NUMBERS.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+	
+	int a ;
+	
+	printf("enter a number between 65 to 90 and 97 to 122");
+	
+	printf("\n");
+	
+	scanf("%d",&a);
+	
+	printf("\n");
+	
+    printf("the corresponding ASCII character for %d is %c",a,a);
+	
+}
