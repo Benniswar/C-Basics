975fe73359bdf356606412cc3e38c7d8b4f8049f
Benniswar <92287642+Benniswar@users.noreply.github.com>
2021-11-15 10:50:47 +0530

    Create DECODE.C

---
+++ DECODE.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+	int A,B,C,D,E;
+	printf("INPUT:\n");
+	scanf("%d%d%d%d%d",&A,&B,&C,&D,&E);
+	printf("OUTPUT:\n");
+	printf("%c%c%c%c%c",A+63,B+63,C+63,D+63,E+63);
+    
+	printf("\nINPUT:\n");
+	scanf("%d%d%d%d%d",&A,&B,&C,&D,&E);
+	printf("OUTPUT:\n");
+	printf("\n%c%c%c%c%c",A+63,B+63,C+63,D+63,E+63);
